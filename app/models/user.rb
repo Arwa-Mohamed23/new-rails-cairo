be4c2aae06b94ebfcb21dc0548020e5c68f0872a
Arwa Mohammed <arwamohmd.32@gmail.com>
2025-05-19 01:50:42 +0300

    Add creator association to posts and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :created_posts, foreign_key: "creator_id", class_name: "Post"
 end
---
+++ db/migrate/20250518223754_add_creator_id_to_posts.rb
@@ -0,0 +1,6 @@
+class AddCreatorIdToPosts < ActiveRecord::Migration[8.0]
+  def change
+    add_column :posts, :creator_id, :integer
+    add_foreign_key :posts, :users, column: :creator_id
+  end
+end
